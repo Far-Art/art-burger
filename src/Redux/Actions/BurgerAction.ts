5e4797527c50d0bacb74dc10492eb10abf56d2b3
Artur Farmanov <arturfarmanov91@gmail.com>
2021-10-18 20:02:08 +0300

    added IngredientsActionType

--- src/Redux/Actions/BurgerAction.ts
+++ src/Redux/Actions/BurgerAction.ts
@@ -1,19 +1,24 @@
 import { IngredientType } from "../../Components/BurgerArea/IngredientType";
-import { IngredientsActionType } from "../Action-Types/IngredientsActionType";
 
 export interface BurgerAction {
-    type:IngredientsActionType
+    type: IngredientsActionType
     payload?: IngredientType
 }
 
+export enum IngredientsActionType {
+    ADD = "ADD",
+    REMOVE = "REMOVE",
+    CLEAR = "CLEAR"
+}
+
 export function addIngredientAction(ingredient: IngredientType): BurgerAction {
-    return {type: IngredientsActionType.ADD, payload:ingredient};
+    return { type: IngredientsActionType.ADD, payload: ingredient };
 }
 
 export function removeIngredientAction(ingredient: IngredientType): BurgerAction {
-    return {type: IngredientsActionType.REMOVE, payload:ingredient};
+    return { type: IngredientsActionType.REMOVE, payload: ingredient };
 }
 
 export function clearIngredientsAction(): BurgerAction {
-    return {type: IngredientsActionType.CLEAR};
+    return { type: IngredientsActionType.CLEAR };
 }
