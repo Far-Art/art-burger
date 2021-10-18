7fa5604c2750493a4084e7e5d65dbd6be82d1777
Artur Farmanov <arturfarmanov91@gmail.com>
2021-10-18 19:55:16 +0300

    Deleted scripts folder with all content for being unnecesary

--- src/Scripts/InfoScript.ts
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-function calcCalories(){
-    const calories = document.getElementsByClassName("Calories");
-    const ingredientsList = document.getElementsByClassName("custom-ingredients");
-    
-}
