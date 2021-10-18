31cd09337443b266de10cc4d406920914f9fb43f
Artur Farmanov <arturfarmanov91@gmail.com>
2021-10-18 19:54:44 +0300

    added global logic for burger items

---
+++ src/AppLogic/AppLogic.ts
@@ -0,0 +1,34 @@
+export default class AppLogic {
+    public static ingredients = {
+        cheese: {
+            maxAmount: 7,
+            price: 1,
+            calories: 87
+        },
+        patty: {
+            maxAmount: 5,
+            price: 2.5,
+            calories: 295
+        },
+        salad: {
+            maxAmount: 6,
+            price: 0.5,
+            calories: 23
+        },
+        tomato: {
+            maxAmount: 4,
+            price: 0.5,
+            calories: 15
+        },
+        topBun: {
+            maxAmount: 1,
+            price: 0,
+            calories: 100
+        },
+        bottomBun: {
+            maxAmount: 1,
+            price: 0,
+            calories: 50
+        }
+    }
+}
