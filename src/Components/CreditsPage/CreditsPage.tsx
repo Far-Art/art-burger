f7565c85f4e2b7f4bcd6b4caebdcd0442d4ae90d
Artur Farmanov <arturfarmanov91@gmail.com>
2021-10-20 15:09:50 +0300

    credits page added

---
+++ src/Components/CreditsPage/CreditsPage.tsx
@@ -0,0 +1,17 @@
+import "./CreditsPage.css";
+
+export default function CreditsPage(): JSX.Element {
+    return (
+        <div className="CreditsPage">
+            {/* <h2>Credits</h2> */}
+
+            <p>Background provided by
+                <a target="_blank" rel="noreferrer noopener" href="/* https://www.svgbackgrounds.com/ */"> svgbackgrounds.com</a>
+            </p>
+
+            <p>Girl image provided by
+                <a href="https://www.dreamstime.com/vector-color-pop-art-comic-style-illustration-girl-eating-cheeseburger-beautiful-business-woman-holding-big-hamburger-image141847574">Dreamstime ID 141847574 Yanatomashova</a>
+            </p>
+        </div>
+    );
+}
