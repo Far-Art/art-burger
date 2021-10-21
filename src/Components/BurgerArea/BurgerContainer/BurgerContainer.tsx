c191b55d6bafa064139e4400bd94d5299495e0f5
Artur Farmanov <arturfarmanov91@gmail.com>
2021-10-21 11:54:04 +0300

    Burger container reworked

--- src/Components/BurgerArea/BurgerContainer/BurgerContainer.tsx
+++ src/Components/BurgerArea/BurgerContainer/BurgerContainer.tsx
@@ -1,22 +1,38 @@
+import { useAppSelector } from "../../../Redux/Hooks/hooks";
 import { IngredientType } from "../IngredientType";
 import "./BurgerContainer.css";
-import BurgerList from "./IngredientsList";
 
-function BurgerContainer(): JSX.Element {
-    // const burger = new BurgerList({});
+export default function BurgerContainer(): JSX.Element {
+
+    const burgerAppState = useAppSelector((state) => state.burgerAppState);
+
+    function setQuantityStyle() {
+        const quantityMap = new Map<IngredientType, "even" | "">();
+        quantityMap.set(IngredientType.CHEESE, "");
+        quantityMap.set(IngredientType.PATTY, "");
+        quantityMap.set(IngredientType.SALAD, "");
+        quantityMap.set(IngredientType.TOMATO, "");
+        return function updateMap(ingredient: IngredientType) {
+            if (quantityMap.get(ingredient) === "even") {
+                quantityMap.set(ingredient, "");
+                return `${ingredient}-even`;
+            }
+            quantityMap.set(ingredient, "even");
+            return "";
+        }
+    }
+
+    const ingredientQuantityStyle = setQuantityStyle();
+
     return (
         <div className="BurgerContainer">
-            <ol className="burger">
-                <li id="top-bun"></li>
-            </ol>
-            <ol className="burger">
-                <BurgerList/>
-            </ol>
-            <ol className="burger">
-                <li id="bottom-bun"></li>
-            </ol>
+            <section className="Burger">
+                <div className="top-bun" />
+                <div className="Ingredients">
+                    {[...burgerAppState.ingredients].reverse().map((ingr, index) => <div style={{ zIndex: index * -1 }} className={`${ingr} ${ingredientQuantityStyle(ingr)}`} key={index} />)}
+                </div>
+                <div className="bottom-bun" />
+            </section>
         </div>
     );
 }
-
-export default BurgerContainer;
