da9af6cf5fa9dd757f5c4aacf0dd00cff014c408
Artur Farmanov <arturfarmanov91@gmail.com>
2021-10-21 11:56:42 +0300

    reworked burger control panel

--- src/Components/ControlPanel/BurgerControlPanel/BurgerControlPanel.tsx
+++ src/Components/ControlPanel/BurgerControlPanel/BurgerControlPanel.tsx
@@ -1,27 +1,19 @@
 import ButtonContainer from "../ButtonContainer/ButtonContainer";
 import "./BurgerControlPanel.css";
-
 import cheeseIcon from '../../../Assets/Icons/cheese-svgrepo-com.svg';
 import saladIcon from '../../../Assets/Icons/salad-svgrepo-com.svg';
 import tomatoIcon from '../../../Assets/Icons/tomato-svgrepo-com.svg';
 import meatIcon from '../../../Assets/Icons/meat-burger-svgrepo-com.svg';
 import { IngredientType } from "../../BurgerArea/IngredientType";
-import store from "../../../Redux/Store";
-import { clearIngredientsAction } from "../../../Redux/Actions/BurgerAction";
 
-function BurgerControlPanel(): JSX.Element {
+export default function BurgerControlPanel(): JSX.Element {
     return (
         <div className="BurgerControlPanel">
-			<ButtonContainer ingredient={IngredientType.CHEESE} icon={cheeseIcon} />
+            <ButtonContainer ingredient={IngredientType.CHEESE} icon={cheeseIcon} />
             <ButtonContainer ingredient={IngredientType.SALAD} icon={saladIcon} />
             <ButtonContainer ingredient={IngredientType.TOMATO} icon={tomatoIcon} />
             <ButtonContainer ingredient={IngredientType.PATTY} icon={meatIcon} />
-            
-            <button className="clear-btn" onClick={() => {
-                store.dispatch(clearIngredientsAction());
-            }}>Clear</button>
+            <ButtonContainer clear={true} ingredient={IngredientType.PATTY} />
         </div>
     );
 }
-
-export default BurgerControlPanel;
