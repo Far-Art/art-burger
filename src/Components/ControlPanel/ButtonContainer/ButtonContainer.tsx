3b81c1a60c625c7076039756223a234a6f728953
Artur Farmanov <arturfarmanov91@gmail.com>
2021-10-21 11:57:20 +0300

    button container added disabled style, all around reworked

--- src/Components/ControlPanel/ButtonContainer/ButtonContainer.tsx
+++ src/Components/ControlPanel/ButtonContainer/ButtonContainer.tsx
@@ -1,62 +1,83 @@
-import React from "react";
-import { Component } from "react";
-import { classicNameResolver } from "typescript";
-import { addIngredientAction, removeIngredientAction } from "../../../Redux/Actions/BurgerAction";
+import AppLogic from "../../../AppLogic/AppLogic";
+import { addIngredientAction, clearIngredientsAction, removeIngredientAction } from "../../../Redux/Actions/BurgerAction";
+import { useAppSelector } from "../../../Redux/Hooks/hooks";
 import store from "../../../Redux/Store";
 import { IngredientType } from "../../BurgerArea/IngredientType";
 import "./ButtonContainer.css";
 
-interface Values{
-    ingredient:IngredientType;
-    icon:string;
+interface ButtonContainerProps {
+    ingredient: IngredientType;
+    icon?: string;
+    clear?: boolean;
 }
 
-function ButtonContainer(props:Values): JSX.Element {
-    store.subscribe(()=>{});
+export default function ButtonContainer(props: ButtonContainerProps): JSX.Element {
+
+    const ingredientAmount = useAppSelector(state => state.burgerAppState);
+
+    function setDisabledStyle(action: "add" | "remove") {
+        const amount = ingredientAmount.amount[props.ingredient];
+        switch (action) {
+            case "add":
+                if (amount === AppLogic.ingredients[props.ingredient].maxAmount) {
+                    return "disabled";
+                }
+                break;
+            case "remove":
+                if (amount === 0) {
+                    return "disabled";
+                }
+                break;
+            default:
+                return "";
+        }
+    }
+
+    function render() {
+        if (props.clear) {
+            return (<button
+                className={`control-btn clear-btn ${ingredientAmount.amount.cheese === 0 &&
+                    ingredientAmount.amount.patty === 0 &&
+                    ingredientAmount.amount.salad === 0 &&
+                    ingredientAmount.amount.tomato === 0 && "disabled"}`}
+                onClick={() => {
+                    store.dispatch(clearIngredientsAction());
+                }}>Clear</button>);
+        }
+        return (
+            <>
+                <button
+                    className={`${("remove-" + props.ingredient)} control-btn left-btn ${setDisabledStyle("remove")}`}
+                    onClick={() => {
+                        if (setDisabledStyle("remove") !== "disabled") {
+                            store.dispatch(removeIngredientAction(props.ingredient));
+                        }
+                    }}
+                >
+                    -
+                </button>
+                <div className="text-area">
+                    <img className="ingredient-icon" src={props.icon} alt={props.ingredient} />
+                    {props.ingredient}
+                </div>
+                <button
+                    className={`${("add-" + props.ingredient)} control-btn right-btn ${setDisabledStyle("add")}`}
+                    onClick={() => {
+                        if (setDisabledStyle("add") !== "disabled") {
+                            store.dispatch(addIngredientAction(props.ingredient));
+                        }
+                    }}
+                >
+                    +
+                </button>
+            </>
+        );
+    }
+
     return (
         <div className="ButtonContainer">
-			<button 
-                id={("remove-" + props.ingredient)} 
-                className={removeIngredient(props.ingredient)}
-                // className="control-btn left-btn"
-                onClick={() => removeIngredient(props.ingredient)}
-                >
-                -
-            </button>
-            <div id="text-area">
-                <img src={props.icon} alt="ingredient-icon"/>
-                {props.ingredient}
-            </div>
-            <button 
-                id={("add-" + props.ingredient)} 
-                className="control-btn right-btn"
-                onClick={() => addIngredient(props.ingredient)}
-                >
-                +
-            </button>
+            {render()}
         </div>
     );
 }
 
-function addIngredient(ingredient: IngredientType){
-    
-    const classes = "control-btn right-btn";
-    let ingredientCount = store.getState().burgerState.ingredients.filter(item => item === ingredient);
-    // if(ingredientCount.length === 0){
-    //     return classes + " disabled";
-    // }
-    store.dispatch(addIngredientAction(ingredient));
-    // return classes + " enabled";
-}
-
-function removeIngredient(ingredient: IngredientType){
-    const classes = "control-btn left-btn";
-    let ingredientCount = store.getState().burgerState.ingredients.filter(item => item === ingredient);
-    if(ingredientCount.length === 0){
-        // return classes + " disabled";
-    }
-    store.dispatch(removeIngredientAction(ingredient));
-    return classes + " enabled";
-}
-
-export default ButtonContainer;
