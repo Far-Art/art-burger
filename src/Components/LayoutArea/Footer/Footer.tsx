6a1f2f8be6267c4f9a7b36b9f48eb1497806fe9b
Artur Farmanov <arturfarmanov91@gmail.com>
2021-10-21 11:57:37 +0300

    footer reworked, added credits

--- src/Components/LayoutArea/Footer/Footer.tsx
+++ src/Components/LayoutArea/Footer/Footer.tsx
@@ -1,11 +1,32 @@
+import { useState } from "react";
+import CreditsPage from "../../CreditsPage/CreditsPage";
 import "./Footer.css";
 
-function Footer(): JSX.Element {
+export default function Footer(): JSX.Element {
+
+    const [hidden, setHidden] = useState<"hidden" | "">("hidden");
+
     return (
         <div className="Footer">
-			<a href="https://far-art-portfolio.herokuapp.com/"><p>All rights reserved to &copy; Artur Farmanov</p></a>
+            <a href="https://far-art-portfolio.netlify.app/home"><p>All rights reserved to &copy; Artur Farmanov</p></a>
+
+            <div
+                className="ToggleCredits"
+            >
+                <p onClick={() => {
+                    if (hidden === "hidden") {
+                        setHidden("");
+                    } else {
+                        setHidden("hidden");
+                    }
+                }}>Credits</p>
+            </div>
+            <div
+                className={`Credits ${hidden}`}
+                onBlur={() => setHidden("hidden")}
+            >
+                <CreditsPage />
+            </div>
         </div>
     );
 }
-
-export default Footer;
